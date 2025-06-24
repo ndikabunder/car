72a5fdec73a9161290c55acc1068fa72fae5568a
ndikabunder <ndikabunder@gmail.com>
2025-06-24 11:11:24 +0700

    Memberikan delay ketika saat bertabrakan dan finish agar tidak langsung restart

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,18 +4,24 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     CapsuleCollider2D playerHead;
+    [SerializeField] float loadDelay = 1f;
 
     void Start()
     {
         playerHead = GetComponent<CapsuleCollider2D>();
-    }  
- 
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Ground" && playerHead.IsTouching(other.collider))
         {
             Debug.Log("Hit my Head!!");
-            SceneManager.LoadScene(0); // Reload the current scene
+            Invoke("ReloadScene", loadDelay); // Reload the scene after the specified delay
         }
+    }
+    
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -3,13 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 1f; // Delay before reloading the scene
+
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
             Debug.Log("Player has crossed the finish line!");
             // You can add more logic here, like updating the game state or triggering an event.
-            SceneManager.LoadScene(0); // Reload the current scene
+            Invoke("ReloadScene", loadDelay); // Reload the scene after the specified delay
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
